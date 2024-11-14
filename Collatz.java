0f832ca2e636b84545e1dd61dc9b1e4839e789bf
Tomer Elankry <tomerelankry@Tomers-MacBook-Pro.local>
2024-11-14 22:21:44 +0200

    completed

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,40 @@
 // Demonstrates the Collatz conjecture.
+
+import java.io.Console;
+
 public class Collatz {
-	public static void main(String args[]) {
-	    // Replace this comment with your code
+	public static void main(String args[])
+    {
+	   int seed1 = Integer.parseInt(args[0]); //Integer that accepts the seed
+	   int seed2=1;
+	   int i=0;
+	   boolean b1 = false;
+	   String vc1 = args[1];
+	   if(vc1.equals("v"))
+	   {
+	    for(int j = 1;j<=seed1;j++)
+	     {
+	        while(!b1)
+		      {
+				System.out.print(seed2+" ");
+				if(seed2%2==0) seed2 = seed2/2;
+				else seed2 = (seed2*3) + 1;
+				if(seed2==1) 
+				{
+					System.out.print(seed2);
+					b1 = true;
+				}
+				i++;
+	         }
+			b1 = false;
+			seed2 = seed2 +j;
+			System.out.print("(" +(i+1)+")");
+			System.out.println();
+			i=0;
+			
+	     }
+		 System.out.println("Every one of the first " + seed1 +" hailstone sequences reached 1.");
+	   }
+	   else System.out.println("Every one of the first " + seed1 +" hailstone sequences reached 1.");
 	}
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,37 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        String s1 = args[0];
+        int h1 = Integer.parseInt(""+s1.charAt(0)+s1.charAt(1)+"");
+        int m1 = Integer.parseInt(""+ s1.charAt(3)+s1.charAt(4));
+        int addT= Integer.parseInt(args[1]);
+
+        int ttlT= h1 * 60 + m1 + addT;
+        // int totalH= (ttlT / 60);
+        int newH = ttlT / 60 ;
+        int newM = ttlT % 60;
+
+        String h2="";
+        String m2="";
+        if(newH>24) newH=newH%24;
+        if(newH==24) newH= 0;
+        if(newH<10) h2= "0"+ newH;
+        else h2 = ""+newH;
+        if(newM<10) m2 = "0" +newM;
+        else m2 = ""+ newM;
+        
+
+        System.out.println(h2+":"+m2);
+
+
+
+
+        
+
+        
+        
+        
+        
+
+
     }
 }
